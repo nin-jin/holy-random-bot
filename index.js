92e25d2cda9641e698ad92f66aeb0bf99a524326
nin-jin <sairi-na-tenshi@ya.ru>
2019-11-09 15:23:28 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,12 +2,42 @@ function rand() {
   return Math.round(Math.random());
 }
 
-let { x, y } = API.getCurrentPosition();
+let limit = API.getActionPointsCount
+let pos = API.getCurrentPosition();
+let size = API.getArenaSize()
+let enemies = API.getEnemies().map( en => en.position )
 
-if (rand()) {
-  x = rand() ? x + 1 : x - 1;
-} else {
-  y = rand() ? y + 1 : y - 1;
+function diff( a , b ) {
+  return Math.abs(a.x-b.x) + Math.abs(a.y-b.y)
 }
 
-API.move(x, y);
+function danger( pos ) {
+  return enemies.some( en => {
+    return diff( pos , en.position ) < 3
+  } )
+}
+
+let go
+
+for(let en of enemies ) {
+  if( diff( n, pos ) <= limit ) {
+    go = en
+    break
+  }
+}
+
+if(!go) {
+   const opts = []
+   for( let x = 0 ; ++x ; x < size )for( let y = 0 ; ++y ; x <= size ) {
+     if( diff(pos,{x,y}) > 2) continue
+     if(danger({x,y})) continue
+     opts.push({x,y})
+   }
+  const i = Math.floor(Math.random() * opts.length)
+  go = opts[i] || pos
+}
+
+API.move( go.x , go.y )
+
+
+
